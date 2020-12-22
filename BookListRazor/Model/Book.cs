48d862940fa5c81b3d9eda207ad1a46cdf9e0ba0
Akash Verma <akashv1507@gmai.com>
2020-12-23 00:15:59 +0530

    Add project files.

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="chal nikal sahi se naam likh")]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
